b7ac107f092672d963ac83cce3beafbce9fc4e92
jon <cityofsmiles@gmail.com>
2020-04-16 14:34:29 +0800

    Edited installer

--- install-algeb-solver.sh
+++ install-algeb-solver.sh
@@ -8,9 +8,7 @@ pip install sympy
 
 wget https://github.com/cityofsmiles/calculators/raw/master/algeb-solver.zip
 
-#unzip ./algeb-solver.zip
-
-tar -xzvf algeb-solver.tar.gz
+tar -xzvf ./algeb-solver.tar.gz
 
 echo "alias algeb='bash ~/algeb-solver/algeb-solver.sh'" >> ~/.bashrc
 
